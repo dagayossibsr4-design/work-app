736b1a14365e58a87f154f445db4c14dc0288d4b
dagayossibsr4-design <dagayossibsr4@gmail.com>
2026-08-20 13:42:36 +0300

    fix trpc url and babel

--- babel.config.js
+++ babel.config.js
@@ -5,9 +5,6 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: [
-      "react-native-worklets/plugin",
-      "react-native-reanimated/plugin",
-    ],
+    plugins: ["react-native-worklets/plugin"],
   };
 };
