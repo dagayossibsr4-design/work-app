c487317110d6c9d75717a90d42598ec4faeb924d
dagayossibsr4-design <dagayossibsr4@gmail.com>
2026-08-22 21:45:03 +0300

    Restore clean layout with workout provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,13 @@
-import React from "react";
 import { Stack } from "expo-router";
-import { WorkoutProvider } from "../context/workout-context";
-import { AuthProvider } from "../context/auth-context";
+import { WorkoutProvider } from "@/context/workout-context";
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <WorkoutProvider>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="register" options={{ headerShown: false }} />
-        </Stack>
-      </WorkoutProvider>
-    </AuthProvider>
+    <WorkoutProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="register" options={{ headerShown: false }} />
+      </Stack>
+    </WorkoutProvider>
   );
 }
